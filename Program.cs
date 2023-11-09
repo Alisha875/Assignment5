ac14a8b8eed7237bf4ac9d5d870a1f45fa61d6bf
Alisha875 <alishanayak001@gmail.com>
2023-11-09 14:29:48 +0530

    Assignment5

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "gmaill";
+            int age = 25; // Replace with your actual age
+            double height = 175.5; // Replace with your actual height in centimeters
+
+            // Display the variables
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Height: " + height + " cm");
+            Console.ReadKey();
+        }
+    }
+}
